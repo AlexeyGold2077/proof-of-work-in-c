f021c2b3cb23bc7fc09fb8eb16e5e8f2c9675f91
AlexeyGold <AlexGold2077@yandex.ru>
2023-05-10 22:52:25 +0300

    improved stability, added new features

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "sha256.c"
 #define STR_BUFF_SIZE 1024
 
@@ -35,14 +36,13 @@ char leadZerros(char* str, int num){
 	while(str[call] == '0' && str[call])
 		call++;
 	if(call == num)
-		return call;
+		return num + 1;
 	else
 		return 0;
 }
 
 char* inputGen(char* str1, char* str2){
 	char* ret = (char*)malloc(strlen(str1)+strlen(str2));
-	// printf("strlen(str1)+strlen(str2) = %i\n", strlen(str1)+strlen(str2));
 	int iter = 0;
 	while(str1[iter]){
 		ret[iter] = str1[iter];
@@ -59,17 +59,22 @@ char* inputGen(char* str1, char* str2){
 
 int main(int argc, char const *argv[])
 {
-	char strBuff[1024];
+	char strBuff[STR_BUFF_SIZE];
 	printf("enter your string: ");
 	scanf("%1023s", &strBuff);
 	int ldZerrs = 0;
 	printf("enter the number of leading zeros: ");
 	scanf("%i", &ldZerrs);
-	printf("wait, magic happens...\n");
+	if(ldZerrs > 64){ printf("ERROR: number of leading zeros is to big!\n"); return 1; }
+	if(ldZerrs < 0){ printf("ERROR: number of leading zeros cannot be negative!\n"); return 2; }
+	printf("wait...\n\n");
+
+	long int timeBuffStart = time(NULL);
 	char* numBuff = NULL;
 	char* shaOutput = NULL;
 	char* tmpInput = NULL;
 	unsigned long long num = 0;
+
 	do{
 		free(shaOutput), shaOutput = NULL;
 		free(numBuff), numBuff = NULL;
@@ -78,13 +83,9 @@ int main(int argc, char const *argv[])
 		tmpInput = inputGen(strBuff, numBuff);
 		shaOutput = SHA256(tmpInput);
 	}while(!leadZerros(shaOutput, ldZerrs));
-	printf("\ncongratulations!\nyour string is: '%s'\nyour hash is: '%s'\n", tmpInput, shaOutput);
+	long int timeBuffEnd = time(NULL);
 
+	printf("your string is: '%s'\nyour hash is: '%s'\ntime: %ldmin. %ldsec.\n", 
+		tmpInput, shaOutput, (timeBuffEnd-timeBuffStart)/60, (timeBuffEnd-timeBuffStart)%60);
 	return 0;
 }
-
-
-// printf("strBuff = %s\n", strBuff);
-// printf("numBuff = %s\n", numBuff);
-// printf("tmpInput = %s\n", tmpInput);
-// printf("shaOutput = %s\n\n", shaOutput);
